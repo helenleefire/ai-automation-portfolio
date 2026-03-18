e1faf2a2d9ea10ef00cb8e49c1040a60dee3238d
flyingfiretruck <helenleefire@gmail.com>
2026-03-17 18:51:06 -0700

    first assignment

---
+++ week1/summarizer_agent.py
@@ -0,0 +1,33 @@
+# day 2 - build a summarizer agent with pydantic structured output
+# define output schema
+# build agent
+# test
+# push
+import os
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+from langchain.chat_models import init_chat_model
+from langchain.agents import create_agent
+
+load_dotenv()
+
+os.environ["ANTHROPIC_API_KEY"] = os.getenv('ANTHROPIC_API_KEY')
+
+class Summarize(BaseModel) :
+  summary: str = Field("Short summary of article")
+  key_points: list[str] = Field("Article's key points")
+  tone: str = Field("The tone used for summarizing")
+
+model = init_chat_model("claude-sonnet-4-6")
+
+agent = create_agent(
+  model=model,
+  system_prompt="Return a structured and concise summary",
+  response_format= Summarize
+)
+
+result = agent.invoke({
+  "messages" : [{"role": "user", "content":"Summarize this article: https://docs.langchain.com/oss/python/langchain/philosophy"}]
+})
+
+print(result["structured_response"])
