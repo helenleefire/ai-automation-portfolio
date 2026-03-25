be7cd6b15078b102aace4f05072b4fe9674b1227
flyingfiretruck <helenleefire@gmail.com>
2026-03-24 18:58:53 -0700

    practiced storing chunked and embedded document in chroma

---
+++ week_2/character_split_practice.py
@@ -0,0 +1,22 @@
+from langchain_community.document_loaders.word_document import Docx2txtLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+
+loader = Docx2txtLoader("week_2/sample_data/ipsum.docx")
+data = loader.load()
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+texts = text_splitter.split_documents(data)
+
+vectorstore = Chroma(
+  collection_name="my_collection",
+  embedding_function=embeddings,
+  persist_directory="./chroma_langchain_db",
+)
+
+vectorstore.add_documents(texts)
+
+print(texts[0])
