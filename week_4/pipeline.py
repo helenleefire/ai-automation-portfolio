96c05529b1f9a649018890cf9c0a8eb85d834677
flyingfiretruck <helenleefire@gmail.com>
2026-04-14 18:45:37 -0700

    added logging

--- week_4/pipeline.py
+++ week_4/pipeline.py
@@ -1,42 +1,50 @@
 import os
+import anthropic
+import asyncio
+import logging
 from dotenv import load_dotenv
 from pydantic import BaseModel, Field
 from anthropic import AsyncAnthropic
-import asyncio
 
 load_dotenv()
 
 client = AsyncAnthropic(api_key=os.getenv('ANTHROPIC_API_KEY'))
-
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='pipeline.log', level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 class APIResponse(BaseModel) :
     title : str = Field(description="Title of response in a short sentence")
     summary : str = Field(description="Summary of the answer given to user in at most 3 sentences but shorter the better")
     answer : str = Field(description="Full answer for the user that is at most two paragraphs but shorter the better")
-    
+
 async def main() -> None:
     print ("Ask anything to your agent. Enter stop to end. \n")
-
     while True:
         question = input("Your question: ").strip()
         if question.lower() == "stop" :
             break
-        
-        answer = await client.messages.create(
-            max_tokens=1000,
-            messages=[
-                {
-                    "role":"user",
-                    "content": question,
-                }
-            ],
-            model="claude-opus-4-6",
-            tools=[{
-                "name": "format_response",
-                "description": "Format the response",
-                "input_schema": APIResponse.model_json_schema()
-            }],
-            tool_choice={"type": "tool", "name": "format_response"}
-        )
-        print(f"\nAgent answer: {answer.content[0].input} \n\n Enter stop to end.")
-
+        try:
+            logger.info('Calling API')
+            answer = await client.messages.create(
+                max_tokens=1000,
+                messages=[
+                    {
+                        "role":"user",
+                        "content": question,
+                    }
+                ],
+                model="claude-opus-4-6",
+                tools=[{
+                    "name": "format_response",
+                    "description": "Format the response",
+                    "input_schema": APIResponse.model_json_schema()
+                }],
+                tool_choice={"type": "tool", "name": "format_response"}
+            )
+            logger.info(f'Succes!')
+            print(f"\nAgent answer: {answer.content[0].input}\n")
+        except (anthropic.AuthenticationError, TypeError) as e :
+            logger.error(f'Authentication failed\n {e}')
+        except (anthropic.APIConnectionError, TypeError) as e:
+            logger.error(f'Server could not be reached\n {e}' )
+        print("Enter stop to end")
 asyncio.run(main())
