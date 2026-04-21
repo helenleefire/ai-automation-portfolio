33ad85d2f212fe55e7829b91a8574cbbc76f965c
Helen <helen@MacBookPro.home>
2026-04-20 17:54:22 -0700

    try different document loads and use chat engine

--- week_5/llama.py
+++ week_5/llama.py
@@ -2,14 +2,13 @@
 import os
 from dotenv import load_dotenv
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.core import VectorStoreIndex, Settings
+from llama_index.core import VectorStoreIndex, Settings, SimpleDirectoryReader
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.readers.web import SimpleWebPageReader
 from llama_index.llms.anthropic import Anthropic
 
 load_dotenv()
 api_key=os.getenv("ANTHROPIC_API_KEY")
-
 urls = [
   "https://docs.oracle.com/javase/8/docs/api/java/util/HashMap.html",
   "https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html",
@@ -24,11 +23,12 @@ Settings.llm = Anthropic(
     )
 
 documents = SimpleWebPageReader(html_to_text=True).load_data(urls)
+documents += SimpleDirectoryReader(input_dir="./week_5/sample_data").load_data()
 nodes = SentenceSplitter(chunk_size=1024).get_nodes_from_documents(documents)
 index = VectorStoreIndex(nodes)
 
 
-query_engine = index.as_query_engine()
+chat_engine = index.as_chat_engine(similarity_top_k=5, chat_mode="condense_plus_context", response_mode="tree_summarize")
 
 def main() -> None:
   print("What do you want to ask the agent? Enter stop to end session")
@@ -36,7 +36,7 @@ def main() -> None:
     question = input("Your question: ").strip()
     if question.lower() == "stop":
       break
-    answer = query_engine.query(question)
+    answer = chat_engine.chat(question)
     print(f'source node score: {answer.source_nodes[-1].score}')
     print(f'{answer} \n What else would you like to ask? Enter stop to end session.')
 
